57153bdfb6e8ee55714b6078423e43d389180df0
simonegosetto <simone.gosetto@gmail.com>
2021-08-19 17:38:37 +0200

    potenziamento script

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const axios = require('axios');
 
-const STRING_TO_SEARCH = 'gtx 1070'; // scrivere lowercase
+const ARRAY_TO_SEARCH = [
+    {text: 'gtx 1070', price: 250},
+    {text: 'gtx 1660', price: 250},
+];
 const PRICE_LIMIT = 200;
 
 // SUBITO
-axios
-    .get(`https://www.subito.it/hades/v1/search/items?q=${STRING_TO_SEARCH.split(' ').join('+')}&t=s&qso=false&sort=datedesc&lim=5000&start=0&pe=${PRICE_LIMIT}`) // &c=10
-    .then(result => {
-        // console.log(result.data);
-        if (result.data) {
-            const {ads} = result.data;
-            const finalList = ads
-                .filter(ad => ad.features.filter(attr => attr.uri === '/price')?.length > 0)
-                // .filter(ad => ad.subject.toLowerCase().indexOf(STRING_TO_SEARCH) > -1)
-                .map(ad => {
-                    return {subject: ad.subject, url: ad.urls.default}
-                });
-            console.log(finalList);
-        }
-    })
+const search = ARRAY_TO_SEARCH.map(s => axios.get(`https://www.subito.it/hades/v1/search/items?q=${s.text.split(' ').join('+')}&t=s&qso=false&sort=datedesc&lim=5000&start=0&pe=${s.price}`));
+Promise.all(search).then(result => {
+    const ads = result.map(s => s.data.ads);
+    // console.log(ads);
+    let finalList = [];
+    ads.forEach(ad => {
+        finalList = [...finalList, ...ad];
+    });
+    finalList = finalList
+        .filter(ad => ad.features.filter(attr => attr.uri === '/price')?.length > 0)
+        // .filter(ad => ad.subject.toLowerCase().indexOf(STRING_TO_SEARCH) > -1)
+        .map(ad => {
+            return {subject: ad.subject, url: ad.urls.default}
+        });
+    console.log(finalList);
+})
